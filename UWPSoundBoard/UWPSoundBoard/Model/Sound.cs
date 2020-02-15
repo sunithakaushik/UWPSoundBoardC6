bf31877d150d14d182bf27de06d2c5a5f41bc690
sunithakaushik <34132856+sunithakaushik@users.noreply.github.com>
2020-02-14 19:01:57 -0800

    Initial Commit

---
+++ UWPSoundBoard/UWPSoundBoard/Model/Sound.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundBoard.Model
+{
+    public class Sound
+    {
+        public string Name { get; set; }
+        public SoundCategory Category { get; set; }
+        public string AudioFile { get; set; }
+        public string ImageFile { get; set; }
+        public enum SoundCategory
+        {
+            Animals, 
+            Cartoons,
+            Taunts,
+            Warnings
+        }
+
+        public Sound (string name, SoundCategory category)
+        {
+            Name = name;
+            Category = category;
+            AudioFile = $"/Assets/Audio/{category}/{name}.wav";
+            ImageFile = $"/Assets/Images/{category}/{name}.png";
+        }
+    }
+}
---
+++ UWPSoundBoard/UWPSoundBoard/Model/SoundManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UWPSoundBoard.Model
+{
+    public class SoundManager
+    {
+        private List<Sound> getSounds()
+        {
+            var sounds = new List<Sound>();
+            sounds.Add(new Sound("Cow", Sound.SoundCategory.Animals));
+            sounds.Add(new Sound("Cat", Sound.SoundCategory.Animals));
+
+            sounds.Add(new Sound("Gun", Sound.SoundCategory.Cartoons));
+            sounds.Add(new Sound("Spring", Sound.SoundCategory.Cartoons));
+
+            sounds.Add(new Sound("Clock", Sound.SoundCategory.Taunts));
+            sounds.Add(new Sound("LOL", Sound.SoundCategory.Taunts));
+
+            sounds.Add(new Sound("Ship", Sound.SoundCategory.Warnings));
+            sounds.Add(new Sound("Siren", Sound.SoundCategory.Warnings));
+
+            return sounds;
+        }
+
+    }
+}
