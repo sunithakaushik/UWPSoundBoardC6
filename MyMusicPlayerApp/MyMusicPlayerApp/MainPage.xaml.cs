3f67d6e3d96c5bfa80de633d2f22244484dc69a7
sunithakaushik <34132856+sunithakaushik@users.noreply.github.com>
2020-02-26 14:12:21 -0800

    Updated PlayerPage

--- MyMusicPlayerApp/MyMusicPlayerApp/MainPage.xaml.cs
+++ MyMusicPlayerApp/MyMusicPlayerApp/MainPage.xaml.cs
@@ -18,6 +18,8 @@ using static MyMusicPlayerApp.Model.Music;
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
 using Windows.Storage;
+using Windows.Media.Core;
+using Windows.Media.Playback;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -30,6 +32,8 @@ namespace MyMusicPlayerApp
     {
         private ObservableCollection<Music> audios;
         private List<MenuItem> menuItems;
+        private MediaPlayer mediaPlayer;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -79,7 +83,10 @@ namespace MyMusicPlayerApp
         private void MusicGridView_ItemClick(object sender, ItemClickEventArgs e)
         {
             var music = (Music)e.ClickedItem;
-            MyMediaElement.Source = new Uri(this.BaseUri, music.AudioFile);
+            MyMediaElement.Source = MediaSource.CreateFromUri(new Uri(this.BaseUri, music.AudioFile));
+          /*  mediaPlayer = new MediaPlayer();
+            mediaPlayer = MyMediaElement.MediaPlayer;
+            mediaPlayer.Play();  */         
         }
 
         private void MenuItemsListView_ItemClick(object sender, ItemClickEventArgs e)
@@ -89,78 +96,109 @@ namespace MyMusicPlayerApp
             MusicManager.GetSoundsByCategory(audios, menuItem.Category);
             BackButton.Visibility = Visibility.Visible;
         }
-
-        private async void openFile_Click(object sender, RoutedEventArgs e)
+        private async void User_song_click(object sender, ItemClickEventArgs e)
         {
-            FileOpenPicker fop = new FileOpenPicker();
-            fop.SuggestedStartLocation = PickerLocationId.Desktop;
-            fop.ViewMode = PickerViewMode.Thumbnail;
-            fop.FileTypeFilter.Add(".mp4");
+            var clicked_song = (Music)e.ClickedItem;
+            //if song clicked it will add to media userplaylist                
+            StorageFile file = await KnownFolders.VideosLibrary.GetFileAsync(clicked_song.AudioFile);               
+            if (file != null)                
+            {                    
+                var filestream = await file.OpenAsync(FileAccessMode.Read);
+                // mediaControl is a MediaElement defined in XAML 
+                MyMediaElement.Source = MediaSource.CreateFromStream(filestream, file.ContentType);                   
+                mediaPlayer = new MediaPlayer();                   
+                mediaPlayer = MyMediaElement.MediaPlayer;                    
+                //_mediaPlayer.AutoPlay = false;                    
+                mediaPlayer.Play();
+            //  mediaElement.Source = MediaSource.CreateFromUri(new Uri(this.BaseUri, clicked_song.AudioFormat));
+        }
+    }
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer = MyMediaElement.MediaPlayer;
+            mediaPlayer.Pause();
+            this.Frame.Navigate(typeof(PlayerPage));
+        }
 
-            StorageFile file = await fop.PickSingleFileAsync();
-            if (file != null)
+        private void playSoundButton_Click(object sender, RoutedEventArgs e)
+        {          
+            mediaPlayer.Play();
+        }
+
+
+
+        /*    private async void openFile_Click(object sender, RoutedEventArgs e)
             {
-                IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read);
-                MyMediaElement.SetSource(stream, file.ContentType);
+                FileOpenPicker fop = new FileOpenPicker();
+                fop.SuggestedStartLocation = PickerLocationId.Desktop;
+                fop.ViewMode = PickerViewMode.Thumbnail;
+                fop.FileTypeFilter.Add(".mp4");
+
+                StorageFile file = await fop.PickSingleFileAsync();
+                if (file != null)
+                {
+                    IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read);
+                    MyMediaElement.SetSource(stream, file.ContentType);
+                    MyMediaElement.Play();
+                }
+
+            }
+
+            private void play_Click(object sender, RoutedEventArgs e)
+            {
+                if(MyMediaElement.DefaultPlaybackRate != 1)
+                {
+                    MyMediaElement.DefaultPlaybackRate = 1.0;
+                }
+                MyMediaElement.Play();
+            }
+            private void pause_Click(object sender, RoutedEventArgs e)
+            {
+                MyMediaElement.Pause();
+            }
+
+            private void stop_Click(object sender, RoutedEventArgs e)
+            {
+                MyMediaElement.Stop();
+            }
+            private void mute_Click(object sender, RoutedEventArgs e)
+            {
+                MyMediaElement.IsMuted = !MyMediaElement.IsMuted;
+            }
+            private void backward_Click(object sender, RoutedEventArgs e)
+            {
+                MyMediaElement.DefaultPlaybackRate = -2.0;
+                MyMediaElement.Play();
+            }
+            private void forward_Click(object sender, RoutedEventArgs e)
+            {
+                MyMediaElement.DefaultPlaybackRate = 2.0;
                 MyMediaElement.Play();
             }
 
-        }
+            private void volumePlus_Click(object sender, RoutedEventArgs e)
+            {
+                if (MyMediaElement.IsMuted)
+                {
+                    MyMediaElement.IsMuted = false;
+                }
+                if(MyMediaElement.Volume >= 0)
+                {
+                    MyMediaElement.Volume += .1;
+                }
+            }
 
-        private void play_Click(object sender, RoutedEventArgs e)
-        {
-            if(MyMediaElement.DefaultPlaybackRate != 1)
+            private void volumeMinus_Click(object sender, RoutedEventArgs e)
             {
-                MyMediaElement.DefaultPlaybackRate = 1.0;
-            }
-            MyMediaElement.Play();
-        }
-        private void pause_Click(object sender, RoutedEventArgs e)
-        {
-            MyMediaElement.Pause();
-        }
-
-        private void stop_Click(object sender, RoutedEventArgs e)
-        {
-            MyMediaElement.Stop();
-        }
-        private void mute_Click(object sender, RoutedEventArgs e)
-        {
-            MyMediaElement.IsMuted = !MyMediaElement.IsMuted;
-        }
-        private void backward_Click(object sender, RoutedEventArgs e)
-        {
-            MyMediaElement.DefaultPlaybackRate = -2.0;
-            MyMediaElement.Play();
-        }
-        private void forward_Click(object sender, RoutedEventArgs e)
-        {
-            MyMediaElement.DefaultPlaybackRate = 2.0;
-            MyMediaElement.Play();
-        }
-
-        private void volumePlus_Click(object sender, RoutedEventArgs e)
-        {
-            if (MyMediaElement.IsMuted)
-            {
-                MyMediaElement.IsMuted = false;
-            }
-            if(MyMediaElement.Volume >= 0)
-            {
-                MyMediaElement.Volume += .1;
-            }
-        }
-
-        private void volumeMinus_Click(object sender, RoutedEventArgs e)
-        {
-            if (MyMediaElement.IsMuted)
-            {
-                MyMediaElement.IsMuted = false;
-            }
-            if (MyMediaElement.Volume < 1)
-            {
-                MyMediaElement.Volume -= .1;
-            }
-        }
+                if (MyMediaElement.IsMuted)
+                {
+                    MyMediaElement.IsMuted = false;
+                }
+                if (MyMediaElement.Volume < 1)
+                {
+                    MyMediaElement.Volume -= .1;
+                }
+            }*/
     }
 }
---
+++ MyMusicPlayerApp/MyMusicPlayerApp/PlayerPage.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using static MyMusicPlayerApp.Model.Music;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MyMusicPlayerApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PlayerPage : Page
+    {
+        private ObservableCollection<MusicCategory> categories;
+        private MediaPlayer mediaPlayer;
+        public PlayerPage()
+        {
+            this.InitializeComponent();
+            categories = new ObservableCollection<MusicCategory>();
+            categories.Add(MusicCategory.Classical);
+            categories.Add(MusicCategory.Country);
+            categories.Add(MusicCategory.International);
+            categories.Add(MusicCategory.Others);
+            categories.Add(MusicCategory.Rock);
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void TxtFilePath_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                TextBox tbPath = sender as TextBox;
+
+                if (tbPath != null)
+                {
+                    LoadMediaFromString(tbPath.Text);
+                }
+            }
+        }
+
+        private void LoadMediaFromString(string path)
+        {
+            try
+            {
+                mediaPlayer = new MediaPlayer();
+                Uri pathUri = new Uri(path);
+                mediaPlayer.Source = MediaSource.CreateFromUri(pathUri);
+            }
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
+                {
+                    // handle exception.
+                    // For example: Log error or notify user problem with file
+                }
+            }
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txt_songname.Text))
+            {
+                txt_songname.Text = string.Empty;
+            }
+
+            if (!string.IsNullOrEmpty(txtImagePath.Text))
+            {
+                txtImagePath.Text = string.Empty;
+            }
+
+            if (!string.IsNullOrEmpty(drop_category.Text))
+            {
+                drop_category.Text = string.Empty;
+            }
+        }
+
+    }
+}
